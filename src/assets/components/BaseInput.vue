a67e16d23e9c82887b9cd4eda22ad89a548018f8
matheushagedorn <matheushagedorn@univille.br>
2025-05-08 21:21:11 -0300

    basecheckbox e template do modal

--- src/assets/components/BaseInput.vue
+++ src/assets/components/BaseInput.vue
@@ -1,25 +1,5 @@
 <template>
   <div class="relative mb-5">
-    <label
-      class="mb-2.5 block text-black"
-      :class="{ 'flex items-center gap-2': type === 'checkbox' }"
-    >
-      <template v-if="type === 'checkbox'">
-        <input
-        type="checkbox"
-        :checked="modelValue"
-        :class="`h-5 w-5 rounded border-[1.5px] border-stroke text-primary focus:ring-primary ${customClasses}`"
-        @change="$emit('update:modelValue', $event.target.checked)"
-        />
-        {{ label }}
-        <span v-if="required" class="text-meta-1 text-red-500">*</span>
-      </template>
-      <template v-else>
-        {{ label }}
-        <span v-if="required" class="text-meta-1 text-red-500">*</span>
-      </template>
-    </label>
-
     <div v-if="type === 'password'" class="relative">
       <font-awesome-icon
         v-if="icon"
@@ -42,7 +22,7 @@
       </button>
     </div>
 
-    <div v-else-if="type !== 'checkbox'" class="relative">
+    <div v-else class="relative">
       <font-awesome-icon
         v-if="icon"
         :icon="icon"
