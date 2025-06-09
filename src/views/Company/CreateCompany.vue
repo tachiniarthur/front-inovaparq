ad6156053d86d95aa355f81e9ad66b16f7709779
matheushagedorn <155010@univille.br@MHGTI.univille.br>
2025-06-09 13:02:36 -0300

    tela de criação de empresa

--- src/views/Company/CreateCompany.vue
+++ src/views/Company/CreateCompany.vue
@@ -5,7 +5,7 @@
       <!-- Informações Básicas -->
       <div class="bg-white shadow-md rounded-lg w-full p-8 space-y-6">
         <h2 class="text-lg font-semibold mb-4 border-b pb-2">Informações Básicas</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
           <BaseInput
             v-model="form.name"
             label="Nome da Empresa"
@@ -20,10 +20,88 @@
             placeholder="Digite o CNPJ da empresa"
             required
           />
+          <BaseInput
+            v-model="form.stateSubscription"
+            label="Inscrição Estadual"
+            icon="pen-to-square"
+            placeholder="Digite a inscrição estadual da empresa"
+            required
+          />
+          <BaseInput
+            v-model="form.civicSubscription"
+            label="Inscrição Municipal"
+            icon="pen-to-square"
+            placeholder="Digite a inscrição municipal da empresa"
+            required
+          />
+          <BaseInput
+            v-model="form.corporateName"
+            label="Razão social"
+            icon="id-card"
+            placeholder="Digite a razão social da empresa"
+            required
+          />
+          <BaseInput
+            v-model="form.fantasyName"
+            label="Nome fantasia"
+            icon="id-card"
+            placeholder="Digite o nome fantasia da empresa"
+            required
+          />
+          <BaseInput
+            v-model="form.cnpj"
+            label="Telefone"
+            icon="id-card"
+            placeholder="Digite o telefone da empresa"
+            v-mask="['(##) ####-####'],['(##) #####-####']"
+            required
+          />
+          <BaseInput
+            v-model="form.email"
+            label="E-mail"
+            icon="envelope"
+            placeholder="Digite o e-mail da empresa"
+            type="email"
+            required
+          />
+          <BaseInput
+            v-model="form.website"
+            label="Site (opcional)"
+            icon="globe"
+            placeholder="Digite o site da empresa"
+            type="text"
+            required
+          />
+          <BaseInput
+            v-model="form.foundingDate"
+            label="Data de fundação"
+            icon="envelope"
+            placeholder="Digite a data de fundação da empresa"
+            type="text"
+            v-mask="['##/##/####']"
+            required
+          />
+          <select
+            v-model="form.legalNature"
+            class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+            required
+            >
+            <option value="LTDA">LTDA</option>
+            <option value="MEI">MEI</option>
+            <option value="S/A">S/A</option>
+          </select>
+          <BaseInput
+            v-model="form.email"
+            label="E-mail"
+            icon="envelope"
+            placeholder="Digite o e-mail da empresa"
+            type="email"
+            required
+          />
         </div>
       </div>
       <!-- Documentos -->
-      <div class="bg-white shadow-md rounded-lg w-full p-8 space-y-6">
+      <!-- <div class="bg-white shadow-md rounded-lg w-full p-8 space-y-6">
         <h2 class="text-lg font-semibold mb-4 border-b pb-2">Documentos</h2>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           <BaseInput
@@ -54,7 +132,7 @@
             accept=".pdf,.jpg,.jpeg,.png"
           />
         </div>
-      </div>
+      </div> -->
       <div class="flex justify-end">
         <button
           type="submit"
